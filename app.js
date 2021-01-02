e9d4cf93b0c75245d25b9c21ca178e2b677e5a17
DivyanshTamrakar <tamrakardivyansh189@gmail.com>
2021-01-03 01:12:37 +0530

    feat : add Todo list

--- app.js
+++ app.js
@@ -17,28 +17,35 @@ todoButton.addEventListener("click", addTodo);
 function addTodo(event) {
     event.preventDefault();
 
-    // create div element 
-    const todoDiv = document.createElement("div");
-    todoDiv.classList.add("todo");
+    if (todoInput.value === "") {
+        alert("Input Can't be Empty");
+    } else {
+        // create div element 
+        const todoDiv = document.createElement("div");
+        todoDiv.classList.add("todo");
 
-    // create li 
-    const newTodo = document.createElement("li");
-    newTodo.innerText = "Hey";
-    newTodo.classList.add("todo-item");
-    todoDiv.appendChild(newTodo);
-    // completing button
-    const completedButton = document.createElement("button");
-    completedButton.innerHTML = '<i class="fas fa-check"></i>';
-    completedButton.classList.add("complete-btn");
-    todoDiv.appendChild(completedButton);
-    // Delete button
-    const trash = document.createElement("button");
-    trash.innerHTML = '<i class="fas fa-trash"></i>';
-    trash.classList.add("trash-btn");
-    todoDiv.appendChild(trash);
+        // create li 
+        const newTodo = document.createElement("li");
+        newTodo.innerText = todoInput.value;
+        newTodo.classList.add("todo-item");
+        todoDiv.appendChild(newTodo);
+        // completing button
+        const completedButton = document.createElement("button");
+        completedButton.innerHTML = '<i class="fas fa-check"></i>';
+        completedButton.classList.add("complete-btn");
+        todoDiv.appendChild(completedButton);
+        // Delete button
+        const trash = document.createElement("button");
+        trash.innerHTML = '<i class="fas fa-trash"></i>';
+        trash.classList.add("trash-btn");
+        todoDiv.appendChild(trash);
 
-    todoList.appendChild(todoDiv);
+        todoList.appendChild(todoDiv);
+        todoInput.value = "";
+
+        
 
 
+    }
 
 }
